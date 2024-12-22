fd23cd5ae65a634db94435a5537fc0664517df3f
Ly Heng <hengly12>
2024-12-22 16:55:12 +0700

    haha��

--- index.js
+++ index.js
@@ -19,7 +19,33 @@ app.get('/todo', (req, res) => {
   
     res.status(200).send(dataQuery); 
 });
-  
+
+app.post('/todo', (req, res) => {
+    const { id, title, completed } = req.query; 
+    const newTodo = { id: Number(id), title, completed: Boolean(completed) }; 
+    console.log(newTodo, 'added');
+    data.push(newTodo);
+});
+
+app.put('/todo', (req, res) => {
+    const { id, title, completed } = req.query; 
+    const todoIndex = data.findIndex((item) => item.id === Number(id));
+    if (todoIndex !== -1) {
+      data[todoIndex] = { id: Number(id), title, completed: Boolean(completed) };
+    }
+    console.log(data[todoIndex]);
+});
+
+app.delete('/todo', (req, res) => {
+    const { id } = req.query; 
+    const todoIndex = data.findIndex((item) => item.id === Number(id));
+    if (todoIndex !== -1) {
+      data.splice(todoIndex, 1);
+    }
+    console.log(data[todoIndex], 'deleted');
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
